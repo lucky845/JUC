9f2daa270a60224ba57ed2de209ecc21d020ea08
lucky845 <lc1447545564@gmail.com>
2022-03-28 17:38:21 +0800

    测试高并发情况下单线程与多线程的时间消耗情况

---
+++ src/main/java/com/atguigu/part01/Test01_ConcurrencyTest.java
@@ -0,0 +1,54 @@
+package com.atguigu.part01;
+
+/**
+ * @author lucky845
+ * @date 2022年03月28日 17:09
+ */
+public class Test01_ConcurrencyTest {
+
+    /**
+     * 执行次数
+     */
+    private static final long count = 1000001L;
+
+    public static void main(String[] args) throws InterruptedException {
+        // 并发计算
+        concurrency();
+        // 单线程计算
+        serial();
+    }
+
+    private static void concurrency() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(() -> {
+            int a = 0;
+            for (long i = 0; i < count; i++) {
+                a += 5;
+            }
+            System.out.println(a);
+        });
+        thread.start();
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        thread.join();
+        long time = System.currentTimeMillis() - start;
+        System.out.println("concurrency :" + time + "ms, b=" + b);
+    }
+
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (long i = 0; i < count; i++) {
+            a += 5;
+        }
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial: " + time + "ms, b=" + b + ", a=" + a);
+    }
+
+}
